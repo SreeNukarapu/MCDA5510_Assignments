8ec802efa2ea2ad6654a39fe2c79f956d2bac2c7
danieltpenny <danieltpenny@gmail.com>
2019-03-26 22:23:35 -0300

    Added in customer portal
    Added in customer portal project

---
+++ CustomerPortal/CustomerPortal/WebServiceClient.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace CustomerPortal
+{
+
+    public class WebService
+    {
+        private string getAllURL = "http://localhost:8080/RESTSample/rest/Customers";
+        private string urlParameters = "";
+
+        public void getAllCustomers()
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri(getAllURL);
+
+            // Add an Accept header for JSON format.
+            client.DefaultRequestHeaders.Accept.Add(
+            new MediaTypeWithQualityHeaderValue("application/json"));
+
+            // List data response.
+            HttpResponseMessage response = client.GetAsync(urlParameters).Result;  // Blocking call!
+            if (response.IsSuccessStatusCode)
+            {
+                // Parse the response body. Blocking!
+                var dataObjects = response.Content.ReadAsAsync<IEnumerable<Customer>>().Result;
+                foreach (var d in dataObjects)
+                {
+                    Console.WriteLine("{0}", d.firstName);
+                }
+            }
+            else
+            {
+                Console.WriteLine("{0} ({1})", (int)response.StatusCode, response.ReasonPhrase);
+            }
+        }
+    }
+}
